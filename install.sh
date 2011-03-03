469dd7088772f9ed3d06fe1c7193d84f9ebd5d95
Charles Feduke <cfeduke@Charles-Fedukes-MacBook-Pro.local>
2011-03-02 21:02:39 -0500

    updated install.sh from actual experience

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 
+echo bash
+ln -s ~/bin/dotfiles/bashrc ~/.bashrc
+curl -s https://github.com/xvzf/vcprompt/raw/master/bin/vcprompt > ~/bin/vcprompt
+chmod 755 ~/bin/vcprompt
+
 echo git
 ln -s ~/bin/dotfiles/git/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
